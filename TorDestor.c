89fc2cea18a4cd3cf6af7cc8d0a2e182e5e2068f
David Clark <dfclark18@outlook.com>
2023-11-28 18:38:24 -0500

    Randomizes files and then deletes them

--- TorDestor.c
+++ TorDestor.c
@@ -6,10 +6,31 @@ void wipe(void *ptr, size_t size) {
     memset(ptr, 0, size); 
 }
 
+void random_wipe(FILE *file, size_t size) {
+    rewind(file);
+    for (int i = 0; i < 3; i++) {
+        char *random_data = malloc(size);
+        if (random_data == NULL) {
+            perror("Error allocating random memory");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int j = 0; j < size; j++) {
+            random_data[j] = rand() % 256;
+        }
+
+        fwrite(random_data, 1, size, file);
+        free(random_data);
+        fflush(file);
+    }
+} 
+
 
 
 int main() {
-    FILE *file = fopen("Sensitive.txt", "r+");
+    char *file_name = "Sensitive.txt";
+
+    FILE *file = fopen(file_name, "r+");
 
     fseek(file, 0L, SEEK_END);
 
@@ -22,16 +43,17 @@ int main() {
 
     printf("File contents: %s\n", content);
 
-    wipe(content, size);
+    random_wipe(file, size);
 
     printf("Wiped contents: %s\n", content);
 
 
     fseek(file, 0L, SEEK_SET);
-    fwrite(content, 1, size, file);
 
     fclose(file);
     free(content);
-    
+
+    remove(file_name);
+ 
     return 0;
 }
